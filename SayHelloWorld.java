5b2ce60f92a204cb9decf3277aaae6c942a51b31
unknown <you@example.com>
2019-09-09 21:25:33 +0800

    wrote a SayHelloWorld file

---
+++ SayHelloWorld.java
@@ -0,0 +1,9 @@
+public class SayHelloWorld { 
+    public static void main(String[] args){
+	    helloworld();
+	}
+	public static void helloworld(){
+	    System.out.println("HelloWorld");
+	}
+
+}
